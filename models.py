c497184ff5d35ee1984083666d0f88219599b8d6
Benoit Chesneau <bchesneau@gmail.com>
2007-11-09 18:21:20 +0100

    [svn r3] - add documentation
    --HG--
    branch : trunk

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
+"""
+ Copyright 2007 Benoît Chesneau 
+ Licensed under the Apache License, Version 2.0 (the "License"); 
+ you may not use this file except in compliance with the License. 
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0 
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
@@ -5,6 +20,9 @@ from django.conf import settings
 import md5, random, sys, os, time
 
 class UserAssociation(models.Model):
+    """ 
+    model to manage association between openid and user 
+    """
     openid_url = models.CharField(blank=False, maxlength=255)
     user = models.ForeignKey(User,unique=True)
     
@@ -30,6 +48,9 @@ class UserPasswordQueueManager(models.Manager):
 
 
 class UserPasswordQueue(models.Model):
+    """
+    model for new password queue.
+    """
     user = models.ForeignKey(User, unique=True)
     new_password = models.CharField(maxlength=30)
     confirm_key = models.CharField(max_length=40)
