40d6172412942b48919d686e6e8a4c6d176b8f94
Benoit Chesneau <bchesneau@gmail.com>
2007-12-08 22:06:15 +0100

    [svn r22] maxlength -> max_length commit missing files
    --HG--
    branch : trunk

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 import md5, random, sys, os, time
 
 class Nonce(models.Model):
-    server_url = models.CharField(maxlength=255)
+    server_url = models.CharField(max_length=255)
     timestamp = models.IntegerField()
     salt = models.CharField(max_length=40)
     
@@ -15,12 +15,12 @@ class Nonce(models.Model):
 
     
 class Association(models.Model):
-    server_url = models.TextField(maxlength=2047)
-    handle = models.CharField(maxlength=255)
-    secret = models.TextField(maxlength=255) # Stored base64 encoded
+    server_url = models.TextField(max_length=2047)
+    handle = models.CharField(max_length=255)
+    secret = models.TextField(max_length=255) # Stored base64 encoded
     issued = models.IntegerField()
     lifetime = models.IntegerField()
-    assoc_type = models.TextField(maxlength=64)
+    assoc_type = models.TextField(max_length=64)
     
     def __unicode__(self):
         return u"Association: %s, %s" % (self.server_url, self.handle)
@@ -29,7 +29,7 @@ class UserAssociation(models.Model):
     """ 
     model to manage association between openid and user 
     """
-    openid_url = models.CharField(blank=False, maxlength=255)
+    openid_url = models.CharField(blank=False, max_length=255)
     user = models.ForeignKey(User,unique=True)
     
     def __unicode__(self):
@@ -58,7 +58,7 @@ class UserPasswordQueue(models.Model):
     model for new password queue.
     """
     user = models.ForeignKey(User, unique=True)
-    new_password = models.CharField(maxlength=30)
+    new_password = models.CharField(max_length=30)
     confirm_key = models.CharField(max_length=40)
 
     objects = UserPasswordQueueManager()
