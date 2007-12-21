592e156ed8a1f4004763091b4e9510e5cdc31209
bchesneau <none@none>
2007-12-21 01:42:36 +0100

    [svn r24] fix openid session variable : http://code.google.com/p/django-authopenid/issues/detail?id=1
    now use only one openid at a time
    
    --HG--
    branch : trunk

--- middleware.py
+++ middleware.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 class OpenIDMiddleware(object):
     """
-    Populate request.openid and request.openids with their openid. This comes 
-    eithen from their cookie or from their session, depending on the presence 
-    of OPENID_USE_SESSIONS.
+    Populate request.openid. This comes either from cookie or from
+    session, depending on the presence of OPENID_USE_SESSIONS.
     """
     def process_request(self, request):
-        request.openids = request.session.get('openids', [])
-        if request.openids:
-            request.openid = request.openids[-1] # Last authenticated OpenID
-        else:
-            request.openid = None
+        request.openid = request.session.get('openid', None)
