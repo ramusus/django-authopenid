76715b4a22114ae6443c133c708675e46e2e224e
Benoit Chesneau <bchesneau@gmail.com>
2008-08-09 15:32:30 +0200

    [svn r78] add xrds document for realm identification and other stuff described here : http://www.intertwingly.net/blog/2007/01/03/OpenID-for-non-SuperUsers
    It should also solve issue #37. To have it working just add OpenID middleware to your list of middleware.
    
    --HG--
    branch : trunk

--- django_authopenid/middleware.py
+++ django_authopenid/middleware.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from django_authopenid import mimeparse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 __all__ = ["OpenIDMiddleware"]
 
@@ -9,3 +12,11 @@ class OpenIDMiddleware(object):
     """
     def process_request(self, request):
         request.openid = request.session.get('openid', None)
+    
+    def process_response(self, request, response):
+        if response.status_code != 200 or len(response.content) < 200:
+            return response
+        path = request.get_full_path()
+        if path == "/" and request.META.has_key('HTTP_ACCEPT') and mimeparse.best_match(['text/html', 'application/xrds+xml'], request.META['HTTP_ACCEPT']) == 'application/xrds+xml':
+            return HttpResponseRedirect(reverse('yadis_xrdf'))
+        return response
--- djauthaupenid_example/urls.py
+++ djauthaupenid_example/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 
+
 urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
     (r'^account/', include('django_authopenid.urls')),
     #(r'^admin/', include('django.contrib.admin.urls')),
 )
