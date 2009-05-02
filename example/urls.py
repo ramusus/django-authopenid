c660cd9490f13491e42c615aaf1414d4c6fc6eca
benoitc <none@none>
2009-05-02 10:15:55 +0200

    better homepage for example
    --HG--
    branch : trunk

--- example/urls.py
+++ example/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    (r'^$', 'example.views.home'),
     (r'^account/', include('django_authopenid.urls')),
     (r'^admin/(.*)', admin.site.root),
 )
